3821e674c4c0600591529f0fb24cfc4557fdaa14
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-06-01 18:54:03 -0300

    feat: adiicionou controller para ENDPOINT Post no /login

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import { IUsersService } from '../interfaces/IUserService.interface';
+
+class LoginController {
+  private _IUsersService: IUsersService;
+  constructor(userRepository: IUsersService) {
+    this._IUsersService = userRepository;
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        const message = 'All fields must be filled';
+        return res.status(400).json({ message });
+      }
+
+      const user = await this._IUsersService.login(email, password);
+
+      return res.status(user.code as number).json(user.result);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default LoginController;
