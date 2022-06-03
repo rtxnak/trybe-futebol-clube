e2b8675adf6b3321347c849f8f38a80b0d44a930
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-06-03 18:52:40 -0300

    feat: adicionou funcao create ao MatchService para ENDPOINT POST no /matches

--- app/backend/src/interfaces/IMatchService.interface.ts
+++ app/backend/src/interfaces/IMatchService.interface.ts
@@ -1,5 +1,8 @@
 import { IMatchFindAllResult } from './IMatchFindAllResult.interface';
+import { IMatch } from './IMatch.interface';
+import { IMatchCreateResult } from './IMatchCreateResult.interface';
 
 export interface IMatchService {
   findAll(query: boolean | null): Promise<IMatchFindAllResult>
+  create(match: IMatch): Promise<IMatchCreateResult>
 }
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,5 +1,6 @@
 import { IMatchRepository } from '../interfaces/IMatchRepository.interface';
 import { IMatchFindAllResult } from '../interfaces/IMatchFindAllResult.interface';
+import { IMatch } from '../interfaces/IMatch.interface';
 
 class MatchService {
   private _IMatchRepository: IMatchRepository;
@@ -10,6 +11,13 @@ class MatchService {
   async findAll(query: boolean | null): Promise<IMatchFindAllResult> {
     const result = await this._IMatchRepository.findAll(query);
     return { code: 200, result };
+  }
+
+  async create(match: IMatch) {
+    const matchValue = match;
+    matchValue.inProgress = true;
+    const result = await this._IMatchRepository.create(match);
+    return { code: 201, result };
   }
 }
 
